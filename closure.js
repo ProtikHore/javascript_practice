ad5713980c2a7b88e56843902dd02b7962999469
ProtikHore <protik.hore@gmail.com>
2021-07-03 19:39:56 +0600

    closur running

---
+++ closure.js
@@ -0,0 +1,14 @@
+var sum = function(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(sum(2, 4));
+
+
+var x = 5;
+var y = 6
+var sum2 = function() {
+    return x + y;
+}
+
+console.log(sum2());
